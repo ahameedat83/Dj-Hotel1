871468b2c6c0b67ab08f6d7c31cbfb3aa9acbbe7
abdallah hameedat <ahameedat@DESKTOP-J3487HC.btc.corp>
2020-09-17 03:47:10 +0300

    add posts table in the db

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=2000)
+
+    def __str__ (self):
+        return self.title
+
+
