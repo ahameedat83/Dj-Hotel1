9b7c353ebf4707658dc87ce835e35de9b0172bb7
abdallah hameedat <ahameedat@DESKTOP-J3487HC.btc.corp>
2020-09-17 04:09:24 +0300

    add one filed in the table

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 
 class post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=2000)
+    created_at = models.DateTimeField()
 
     def __str__ (self):
         return self.title
