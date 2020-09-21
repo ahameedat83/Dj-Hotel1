2844d90a982e5b3cb3d281e9d71c7ef05a0bd82e
abdallah hameedat <ahameedat@DESKTOP-J3487HC.btc.corp>
2020-09-21 03:02:39 +0300

    make models,view,new urls related to the main urls

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,24 @@ from django.db import models
 
 # Create your models here.
 
-class post(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(max_length=2000)
-    created_at = models.DateTimeField()
+POST_TYPE = (
 
+('NOTE','NOTE'),
+
+('BIG NOTE','BIG NOTE'),
+
+
+)
+
+
+class post(models.Model):
+    title = models.CharField(max_length=50 , unique=True)
+    content = models.TextField(max_length=2000 , null=True , blank= True)
+    created_at = models.DateTimeField()
+    active = models.BooleanField(default=False)
+    author_mail = models.EmailField(default='')
+    type = models.CharField(choices=POST_TYPE ,default='NOTE', max_length= 20)
+    image = models.ImageField(upload_to='post/')
     def __str__ (self):
         return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import post
 
 # Create your views here.
+
+
+def all_posts(request):
+    all_posts = post.objects.all()
+    return render(request,'post/all_posts.html',{'posts':all_posts})
+
+def single_post(request,id):
+    single_post = post.objects.get(id=id)
+    return render(request,'post/single_posts.html',{'post':single_post})    
